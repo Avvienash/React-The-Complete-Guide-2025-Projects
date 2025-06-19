5a275946b283bfbb85909098af7b1efc3448c2ed
Avvienash <avvienash@gmail.com>
2025-06-19 21:01:24 +1000

    update via cli

--- 02-tic-tac-toe-starting-project/vite.config.js
+++ 02-tic-tac-toe-starting-project/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/ dd
+// https://vitejs.dev/config/ ddd
 export default defineConfig({
   plugins: [react()],
 })
