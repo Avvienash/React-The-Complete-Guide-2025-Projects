489f3922398d5696512367c42bbfba40a941ad3e
Avvienash <avvienash@gmail.com>
2025-08-03 17:53:45 +1000

    publishing

--- 02-tic-tac-toe-starting-project/vite.config.js
+++ 02-tic-tac-toe-starting-project/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/ ddd
 export default defineConfig({
+  base: '/React-The-Complete-Guide-2025-Projects/02-tic-tac-toe-starting-project/',
   plugins: [react()],
 })
