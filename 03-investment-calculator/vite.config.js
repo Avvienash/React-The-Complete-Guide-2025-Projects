3fbee959c7f28e3bea86642f330ea9427fd9d31b
Avvienash <avvienash@gmail.com>
2025-08-03 15:58:04 +1000

    publishing

--- 03-investment-calculator/vite.config.js
+++ 03-investment-calculator/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/React-The-Complete-Guide-2025-Projects/03-investment-calculator/',
   plugins: [react()],
 })
